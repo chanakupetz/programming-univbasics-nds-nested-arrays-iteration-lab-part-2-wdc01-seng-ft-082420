9132e1d935972f412e231e6c5ce123536d650bd5
Chana Kupetz <chanakupetz@gmail.com>
2020-07-29 05:18:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,14 +5,18 @@ def find_min_in_nested_arrays(src)
 
   row_index = 0
   while row_index < src.count do
-    element_index = 0
-    smallest_element = ""
-    while element_index < src[row_index].count do
-      if src[row_index][element_index].length > smallest_element.length
-        smallest_element = src[row_index][element_index]
-      end
+
+    # element_index = 0
+  #  smallest_element = 
+
+  #  while element_index < src[row_index].count do
+  #    if src[row_index][element_index].length > smallest_element.length
+  #      smallest_element = src[row_index][element_index]
+  #    end
+
     element_index += 1
   end
+
     min_temp_week << smallest_element
     row_index += 1
   end
